ab369134fe03450e9e79b845fc864e6e4af8a23b
Pong <vx7vx8vx9@gmail.com>
2026-01-05 10:36:25 +0700

    feat: Ponng create Components Navbar ProductHighlight ProductCategory Footer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,5 +12,49 @@
 // 🛠️ Todo 1 / Step 2: ให้ Export ตัว Component ออกไป ลงในไฟล์นี้ด้านล่าง เพื่อจะไป Import ใน `App.jsx`
 
 //     ==== ตัวอย่าง ====
+import {
+    BeakerIcon,
+    HomeIcon,
+    ShoppingCartIcon,
+    DevicePhoneMobileIcon,
 
+  } from "@heroicons/react/24/solid";
 //     export default Navbar
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+    <div className="container mx-auto flex justify-between items-center">
+      <a href="#" className="text-white text-xl font-semibold">
+        Ecommerce
+      </a>
+      <ul className="flex space-x-4">
+        <li className="flex items-center">
+          <a href="#" className="text-white flex items-center">
+            <HomeIcon className="h-5 w-5 mr-1" />
+            Home
+          </a>
+        </li>
+        <li className="flex items-center">
+          <a href="#" className="text-white flex items-center">
+            <BeakerIcon className="h-5 w-5 mr-1" />
+            Products
+          </a>
+        </li>
+        <li className="flex items-center">
+          <a href="#" className="text-white flex items-center">
+            <ShoppingCartIcon className="h-5 w-5 mr-1" />
+            Cart
+          </a>
+        </li>
+        <li className="flex items-center">
+          <a href="#" className="text-white flex items-center">
+            <DevicePhoneMobileIcon className="h-5 w-5 mr-1" />
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+export default Navbar
